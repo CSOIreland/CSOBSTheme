a8c2a87f45e47830f486eb2ba194d94bc7e99c15
Stephen Lane <stephen.lane@cso.ie>
2023-12-11 16:05:38 +0000

    bootstrap 5.3

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
-const miniCssExtractPlugin = require('mini-css-extract-plugin')
+'use strict'
+
 const path = require('path')
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const autoprefixer = require('autoprefixer')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const miniCssExtractPlugin = require('mini-css-extract-plugin')
+  const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  devtool: 'source-map',
+ devtool: 'source-map',
   entry: './src/js/app.bootstrap.guideline.js',
-  optimization: {
+   optimization: {
     minimizer: [
       new TerserPlugin({
         extractComments: {
@@ -40,36 +44,43 @@ module.exports = {
     static: path.resolve(__dirname, 'dist'),
     port: 8080,
     hot: true
-  },
+  },  
   performance: {
     hints: false,
     maxEntrypointSize: 512000,
     maxAssetSize: 512000
   },
+  plugins: [
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+   new miniCssExtractPlugin()  ],
   module: {
     rules: [
       {
         test: /\.(scss)$/,
         use: [
           {
+             // Extracts CSS for each JS file that includes CSS
             loader: miniCssExtractPlugin.loader
           },
           {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
             loader: 'css-loader', options: { sourceMap: true }
           },
           {
+            // Loader for webpack to process CSS with PostCSS
             loader: 'postcss-loader',
             options: {
               postcssOptions: {
-                plugins: () => [
-                  require('autoprefixer')
+                plugins: [
+                  autoprefixer
                 ]
               }
             }
           },
           {
-            loader: "sass-loader", options: { sourceMap: true }
-          },
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: 'sass-loader', options: { sourceMap: true }
+          }
         ]
       }
     ]
